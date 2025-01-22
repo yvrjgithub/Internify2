94615311dc6f18af669100a6db74ef4f4e0aabfd
yvrjgithub <127379082+yvrjgithub@users.noreply.github.com>
2025-01-22 22:27:02 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://internify2-n7mz.vercel.app',
     credentials:true
 }
 app.use(cors(corsOptions))
