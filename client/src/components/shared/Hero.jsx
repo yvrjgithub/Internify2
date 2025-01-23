b73d2cf12caf6d0c1ebc3310d6f82248e8444390
yvrjgithub <127379082+yvrjgithub@users.noreply.github.com>
2025-01-23 12:33:42 +0530

    v

--- client/src/components/shared/Hero.jsx
+++ client/src/components/shared/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
         <div className='text-center w-6/12 bg-[#CDE4E1] mx-auto rounded-full sm:w-6/12 md:w-3/12'><h1 className='text-xl'>No.1 Internship Portal</h1></div>
         <div className='my-8'><h2 className='mb-4 text-4xl font-bold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl'>Search , Apply &</h2>
         <h2 className='mb-4 text-4xl font-bold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl'>Get your dream <span className='text-[#147fe3]'>Internship</span></h2>
-         <h2 className='text-[#63696E] my-8 text-l mx-4 sm:mx-20 md:mx-56 '>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia perferendis vero pariatur earum, delectus neque itaque odit magnam recusandae aspernatur? Magnam quos quae, autem saepe dolor commodi nostrum quo mollitia!</h2>
+         <h2 className='text-[#63696E] my-8 text-l mx-4 sm:mx-20 md:mx-56 '> Discover countless internship opportunities tailored to your skills and passions. Start your journey toward professional growth today, and take the first step in building your dream career.</h2>
         </div>
         <div className='flex justify-center my-8'>
         <Input className='w-8/12 shadow-md h-10 rounded-full rounded-tr-none rounded-br-none text-xl md:6/12'
